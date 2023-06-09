daebd8aa1e1f5966b72e3b248de1ccd5050a18ea
muzaffarvosilov <109663125+muzaffarvosilov@users.noreply.github.com>
2023-06-09 10:49:01 +0500

    Add files via upload

---
+++ begin2/begin2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace begin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 0;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = a * a;
+
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
