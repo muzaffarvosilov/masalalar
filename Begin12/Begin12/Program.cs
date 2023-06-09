1e0caba914183fa50322d5505705a658c00d7a7b
muzaffarvosilov <109663125+muzaffarvosilov@users.noreply.github.com>
2023-06-09 10:31:39 +0500

    Add files via upload

---
+++ Begin12/Begin12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, p;
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            p = c + a + b;
+
+            Console.WriteLine("c=" + c);
+            Console.WriteLine("p=" + p);
+            Console.ReadKey();
+        }
+    }
+}
